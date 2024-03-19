9f3ae48ce86c90d9bd1a09544830ffe98bddedfc
Arindam Kulshi <akulshi04@gmail.com>
2024-03-19 15:31:12 -0700

    edited test file name

--- OCR/segmentation_template_main.py
+++ OCR/segmentation_template_main.py
@@ -6,24 +6,29 @@ import cv2 as cv
 
 from services.image_segmenter import ImageSegmenter
 
-load_dotenv()
+def main():
+    load_dotenv()
 
-segmentation_template = os.getenv('SEGMENTATION_TEMPLATE_PATH')
-raw_image = os.getenv('RAW_IMAGE_PATH')
-labels_path = os.getenv('LABELS_PATH')
-
-
-
-segmenter = ImageSegmenter(raw_image, segmentation_template,labels_path)
-segments = segmenter.segment()
-
-segment_info = {label: segment_data.shape for label, segment_data in segments.items()}
+    segmentation_template = os.getenv('SEGMENTATION_TEMPLATE_PATH')
+    raw_image = os.getenv('RAW_IMAGE_PATH')
+    labels_path = os.getenv('LABgtELS_PATH')
 
 
 
-nbs_patient_id_image_path = 'nbs_patient_id_image_path.png'
-nbs_cas_id_image_path = 'nbs_cas_id_image_path.png'
+    segmenter = ImageSegmenter(raw_image, segmentation_template,labels_path)
+    segments = segmenter.segment()
 
-# Save the images
-cv.imwrite(nbs_patient_id_image_path , segments['nbs_patient_id'])
-cv.imwrite(nbs_cas_id_image_path, segments['nbs_cas_id'])
+    segment_info = {label: segment_data.shape for label, segment_data in segments.items()}
+
+
+
+    nbs_patient_id_image_path = 'nbs_patient_id_image_path.png'
+    nbs_cas_id_image_path = 'nbs_cas_id_image_path.png'
+
+    # Save the images
+    cv.imwrite(nbs_patient_id_image_path , segments['nbs_patient_id'])
+    cv.imwrite(nbs_cas_id_image_path, segments['nbs_cas_id'])
+
+
+if __name__ == "__main__":
+    main()
