ab5fbbfe52ffa74e271f6090ee32a1e499f9d3be
Arin Kulshi <158501719+arinkulshi-skylight@users.noreply.github.com>
2024-03-26 09:13:16 -0700

    Added logic to segment pdf (#33)
    * initial
    
    * addressed case with no matching pixels
    
    * edited comments
    
    * edited test file name
    
    * delete old test file
    
    * minor typo
    
    * added test to verify that ndarray values are matchin
    
    * edited tests
    
    ---------
    
    Co-authored-by: Arindam Kulshi <akulshi04@gmail.com>

---
+++ OCR/segmentation_template_main.py
@@ -0,0 +1,33 @@
+
+from dotenv import load_dotenv
+import os
+import cv2 as cv
+
+from services.image_segmenter import ImageSegmenter
+
+def main():
+    load_dotenv()
+
+    segmentation_template = os.getenv('SEGMENTATION_TEMPLATE_PATH')
+    raw_image = os.getenv('RAW_IMAGE_PATH')
+    labels_path = os.getenv('LABELS_PATH')
+
+
+
+    segmenter = ImageSegmenter(raw_image, segmentation_template,labels_path)
+    segments = segmenter.segment()
+
+    segment_info = {label: segment_data.shape for label, segment_data in segments.items()}
+    print(segment_info)
+
+
+    nbs_patient_id_image_path = 'nbs_patient_id_image_path.png'
+    nbs_cas_id_image_path = 'nbs_cas_id_image_path.png'
+
+    # Save the images
+    cv.imwrite(nbs_patient_id_image_path , segments['nbs_patient_id'])
+    cv.imwrite(nbs_cas_id_image_path, segments['nbs_cas_id'])
+
+
+if __name__ == "__main__":
+    main()
