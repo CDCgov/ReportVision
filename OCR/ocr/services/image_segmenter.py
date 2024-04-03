ebe8339090ac63bc1f6001fb29f35f7e6d36b746
Zedd Shmais <zdveloper@gmail.com>
2024-04-03 12:35:01 -0500

    Feed image segments into ocr (#62)
    * feed segments into OCR
    
    * remove warning args
    
    * add ocr test
    
    * add handwritten tests
    
    * clean up
    
    * more clean up

--- OCR/ocr/services/image_segmenter.py
+++ OCR/ocr/services/image_segmenter.py
@@ -6,41 +6,41 @@ import os
 
 class ImageSegmenter:
     def __init__(self, raw_image, segmentation_template, labels):
-        
+
         if not os.path.isfile(raw_image) or not os.path.isfile(segmentation_template):
             raise FileNotFoundError("One or more input files do not exist.")
-        
+
         self.raw_image = cv.imread(raw_image)
         if self.raw_image is None:
             raise ValueError(f"Failed to open image file: {raw_image}")
-        
+
         self.segmentation_template = cv.imread(segmentation_template)
         if self.segmentation_template is None:
             raise ValueError(f"Failed to open image file: {segmentation_template}")
-        
-        
+
         self.labels = labels
         self.segments = {}
 
-    def segment(self):
-        with open(self.labels, 'r') as f:
+    def segment(self) -> dict[str, np.ndarray]:
+        with open(self.labels, "r") as f:
             labels = json.load(f)
-        #iterate over the labels
+        # iterate over the labels
         for color, label in labels.items():
-            color = tuple(map(int, color.split(',')))
-            #find indices of the color in the segmentation template where the color matches the expected colors
+            color = tuple(map(int, color.split(",")))
+            # find indices of the color in the segmentation template where the color matches the expected colors
             indices = np.where(np.all(self.segmentation_template == color, axis=-1))
-            #if there are no matching pixels
+            # if there are no matching pixels
             if indices[0].size == 0:
-                raise ValueError(f"No pixels found for color {color} in segmentation template.")
-             #if there are matching pixels
+                raise ValueError(
+                    f"No pixels found for color {color} in segmentation template."
+                )
+            # if there are matching pixels
             if indices[0].size > 0:
-                #Find the x-y coordinates
+                # Find the x-y coordinates
                 y_min, y_max = indices[0].min(), indices[0].max()
                 x_min, x_max = indices[1].min(), indices[1].max()
-                #crop the area and store the image in the dict
-                self.segments[label] = self.raw_image[y_min:y_max+1, x_min:x_max+1]
+                # crop the area and store the image in the dict
+                self.segments[label] = self.raw_image[
+                    y_min : y_max + 1, x_min : x_max + 1
+                ]
         return self.segments
-
-
-
---
+++ OCR/tests/ocr_test.py
@@ -0,0 +1,35 @@
+import pytest
+import os
+from ocr.services.image_segmenter import ImageSegmenter
+from ocr.services.image_ocr import ImageOCR
+
+
+path = os.path.dirname(__file__)
+
+segmentation_template = os.path.join(path, "./assets/form_segmention_template.png")
+raw_image = os.path.join(path, "./assets/form_filled.png")
+raw_image_handwritten = os.path.join(path, "./assets/form_hand_filled.png")
+labels_path = os.path.join(path, "./assets/labels.json")
+
+
+class TestOCR:
+
+    def test_ocr_printed(self):
+        segmenter = ImageSegmenter(raw_image, segmentation_template, labels_path)
+        ocr = ImageOCR()
+
+        results = ocr.image_to_text(segmenter.segment())
+
+        assert results["nbs_patient_id"] == "123555"
+        assert results["nbs_cas_id"] == "SIENNA HAMPTON"
+
+    def test_ocr_handwritten(self):
+        segmenter = ImageSegmenter(
+            raw_image_handwritten, segmentation_template, labels_path
+        )
+        ocr = ImageOCR(model="microsoft/trocr-base-handwritten")
+
+        results = ocr.image_to_text(segmenter.segment())
+
+        assert results["nbs_patient_id"] == "123695"
+        assert results["nbs_cas_id"] == "Harry Potter"
