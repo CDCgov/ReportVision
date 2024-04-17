c90d1e94c29c91b0460f4a385bfb199a220f09bb
Jonathan Chang <jchang641@gmail.com>
2024-04-17 09:43:54 -0700

    Add ruff lint and style checks (#76)
    * Add ruff lint and style checks
    
    * Fix `ruff check`
    
    * Fix `ruff format`
    
    * Only enable for ocr work
    
    * Revert changes to dedupe folder
    
    We aren't linting or formatting dedupe work.

--- OCR/ocr/azure_form_ex.py
+++ OCR/ocr/azure_form_ex.py
@@ -8,14 +8,12 @@ import json
 load_dotenv()
 
 
-endpoint = os.getenv('FORM_RECOGNIZER_ENDPOINT')
-key = os.getenv('FORM_RECOGNIZER_KEY')
-file_path = os.getenv('FORM_RECOGNIZER_FILE_PATH')
-
+endpoint = os.getenv("FORM_RECOGNIZER_ENDPOINT")
+key = os.getenv("FORM_RECOGNIZER_KEY")
+file_path = os.getenv("FORM_RECOGNIZER_FILE_PATH")
 
 
 client = DocumentAnalysisClient(endpoint=endpoint, credential=AzureKeyCredential(key))
-
 
 
 try:
@@ -25,19 +23,18 @@ try:
 except Exception as e:
     print(f"An error occurred: {e}")
 else:
-    with open('output.json', 'w') as json_file:
+    with open("output.json", "w") as json_file:
         for idx, style in enumerate(result.styles):
             if style.is_handwritten:
                 print(f"Document contains handwritten content: {style.confidence}")
 
         for page in result.pages:
             print(f"Page number: {page.page_number} has {len(page.lines)} lines and {len(page.words)} words.")
-            lines = page.lines[:] 
+            lines = page.lines[:]
             lines_content = [line.content for line in lines]
             kv_pairs = {}
             for line in lines_content:
-                if ':' in line:
-                    key, value = line.split(':', 1)
+                if ":" in line:
+                    key, value = line.split(":", 1)
                     kv_pairs[key.strip()] = value.strip()
             json.dump(kv_pairs, json_file, indent=4)
-
