8de679f96cb677089c2c615d54169a83ee56d43e
Arin Kulshi <158501719+arinkulshi-skylight@users.noreply.github.com>
2024-08-06 10:27:29 -0700

    added confidence scores to OCR (#159)
    * added confidence scores to OCR
    
    * edited tests to reflect addition of confidence score
    
    ---------
    
    Co-authored-by: Arindam Kulshi <akulshi04@gmail.com>

--- OCR/ocr/services/image_ocr.py
+++ OCR/ocr/services/image_ocr.py
@@ -1,5 +1,6 @@
-import numpy as np
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+import torch
+import numpy as np
 
 
 class ImageOCR:
@@ -7,16 +8,34 @@ class ImageOCR:
         self.processor = TrOCRProcessor.from_pretrained(model)
         self.model = VisionEncoderDecoderModel.from_pretrained(model)
 
-    def image_to_text(self, segments: dict[str, np.ndarray]) -> dict[str, str]:
-        digitized: dict[str, str] = {}
+    def image_to_text(self, segments: dict[str, np.ndarray]) -> dict[str, tuple[str, float]]:
+        digitized: dict[str, tuple[str, float]] = {}
         for label, image in segments.items():
             if image is None:
                 continue
 
             pixel_values = self.processor(images=image, return_tensors="pt").pixel_values
 
-            generated_ids = self.model.generate(pixel_values)
-            generated_text = self.processor.batch_decode(generated_ids, skip_special_tokens=True)
-            digitized[label] = generated_text[0]
+            with torch.no_grad():
+                outputs = self.model.generate(pixel_values, output_scores=True, return_dict_in_generate=True)
+
+            generated_text = self.processor.batch_decode(outputs.sequences, skip_special_tokens=True)[0]
+
+            # Calculate confidence score
+            confidence = self.calculate_confidence(outputs)
+
+            digitized[label] = (generated_text, confidence)
 
         return digitized
+
+    def calculate_confidence(self, outputs):
+        probs = torch.softmax(outputs.scores[0], dim=-1)
+        max_probs = torch.max(probs, dim=-1).values
+
+        # Calculate the average confidence
+        avg_confidence = torch.mean(max_probs).item()
+
+        # Convert to percentage
+        confidence_percentage = avg_confidence * 100
+
+        return confidence_percentage
