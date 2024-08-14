3f53a81707c2699f1b8ff256fd4c6fe9f53d3946
Zedd Shmais <zedd@skylight.digital>
2024-08-14 16:06:53 -0500

    add anotate page skeleton

--- OCR/frontend/src/componets/Stepper.tsx
+++ OCR/frontend/src/componets/Stepper.tsx
@@ -1,20 +1,32 @@
 import { StepIndicator, StepIndicatorStep } from "@trussworks/react-uswds"
+import {AnnotateStep} from "../utils/constants.ts";
 
-export const Stepper = () => {
+export const Stepper = ({currentStep}: {currentStep: AnnotateStep}) => {
+
+  const stepOrder: AnnotateStep[] = [
+    AnnotateStep.Upload,
+    AnnotateStep.Annotate,
+    AnnotateStep.Save
+  ]
+
+  const determineStatus = (step: AnnotateStep, currentStep: AnnotateStep) => {
+      const currentStepIndex = stepOrder.indexOf(currentStep);
+      const stepIndex = stepOrder.indexOf(step);
+      if(currentStepIndex > stepIndex) return "complete";
+      else if(currentStepIndex === stepIndex) return "current";
+      else return "incomplete";
+    }
+
+
     return (
         <StepIndicator data-testid='step-indicator'  headingLevel="h5" headingProps={{  style: { display: 'none' }}}>
+          {stepOrder.map((step: AnnotateStep) => (
             <StepIndicatorStep
-                label="Upload New Segment"
-                status="current"
+              key={step}
+              label= {step}
+              status={determineStatus(step, currentStep)}
             />
-            <StepIndicatorStep
-                label="Annotate"
-                status="incomplete"
-            />
-            <StepIndicatorStep
-                label="Save Templete"
-                status="incomplete"
-            />
+          ))}
         </StepIndicator>
     )
 }
--- OCR/frontend/src/componets/tests/Stepper.test.tsx
+++ OCR/frontend/src/componets/tests/Stepper.test.tsx
@@ -1,11 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { describe, it, expect } from 'vitest';
-import { Stepper } from '../Stepper';
+import {describe, expect, it} from 'vitest';
+import {Stepper} from '../Stepper';
+import {AnnotateStep} from "../../utils/constants.ts";
 
 describe('Stepper component', () => {
   it('renders the StepIndicator component', () => {
-    render(<Stepper />);
+    render(<Stepper currentStep={AnnotateStep.Upload}/>);
     
     // Check that the StepIndicator is rendered
     const stepIndicator = screen.getByTestId('step-indicator');
