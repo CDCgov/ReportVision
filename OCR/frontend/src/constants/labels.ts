3eb8028213d029f4ca8511f55e47d747a392bbef
Arindam Kulshi <akulshi04@gmail.com>
2024-08-15 08:00:18 -0700

    edited labels file

--- OCR/frontend/src/constants/labels.ts
+++ OCR/frontend/src/constants/labels.ts
@@ -1,25 +1,23 @@
 export const LABELS = {
-    patientInformation: {
-      title: "Patient Information",
-      items: [
-        { name: "First Name", required: true, color: "#FF5733" },
-        { name: "Last Name", required: true, color: "#33FF57" },
-        { name: "DOB", required: false, color: "#3357FF" },
-        { name: "Gender", required: false, color: "#FF33A1" },
-        { name: "Phone Number", required: false, color: "#A133FF" },
-        { name: "Email", required: false, color: "#33FFA1" },
-        { name: "Address", required: false, color: "#FFA133" },
-      ],
-    },
-    organizationInformation: {
-      title: "Organization Information",
-      items: [
-        { name: "Name", required: true, color: "#FF5733" },
-        { name: "Email", required: false, color: "#33FF57" },
-        { name: "Address", required: false, color: "#3357FF" },
-      ],
-    },
-  };
-
-
-  
+  patientInformation: {
+    title: "Patient information",
+    items: [
+      { name: "Date of service", required: true, color: "#0071bc" },
+      { name: "Name", required: true, color: "#4c2c92" },
+      { name: "Date of birth", required: true, color: "#e31c3d" },
+      { name: "Gender", required: false, color: "#2e8540" },
+    ],
+  },
+  organizationInformation: {
+    title: "Organization information",
+    items: [
+      { name: "Name", required: false, color: "#ff7043" },
+    ],
+  },
+  clinicalData: {
+    title: "Clinical data",
+    items: [
+      { name: "Diagnosis", required: false, color: "#ffd54f" },
+    ],
+  },
+};
