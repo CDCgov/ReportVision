00ba6dde6e5aae4f68217bfb177536a27d40876f
Arindam Kulshi <akulshi04@gmail.com>
2024-08-15 07:59:30 -0700

    initial

---
+++ OCR/frontend/src/constants/labels.ts
@@ -0,0 +1,25 @@
+export const LABELS = {
+    patientInformation: {
+      title: "Patient Information",
+      items: [
+        { name: "First Name", required: true, color: "#FF5733" },
+        { name: "Last Name", required: true, color: "#33FF57" },
+        { name: "DOB", required: false, color: "#3357FF" },
+        { name: "Gender", required: false, color: "#FF33A1" },
+        { name: "Phone Number", required: false, color: "#A133FF" },
+        { name: "Email", required: false, color: "#33FFA1" },
+        { name: "Address", required: false, color: "#FFA133" },
+      ],
+    },
+    organizationInformation: {
+      title: "Organization Information",
+      items: [
+        { name: "Name", required: true, color: "#FF5733" },
+        { name: "Email", required: false, color: "#33FF57" },
+        { name: "Address", required: false, color: "#3357FF" },
+      ],
+    },
+  };
+
+
+  
--- OCR/frontend/src/pages/AnnotateTemplate.tsx
+++ OCR/frontend/src/pages/AnnotateTemplate.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { UploadHeader } from '../componets/Header';
+import Sidebar from '../componets/Sidebar';
+import { LABELS } from '../constants/labels';
 
 const AnnotateTemplate: React.FC = () => {
-    return (
-        <div data-testid ='annotate-container'>
-            Hello World
-        </div>
-    );
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', width: '100%', height: '100%', padding: '16px 8px, 8px, 8px' }}>
+      <UploadHeader />
+      <div style={{ display: 'flex', flexDirection: 'row', height: 'calc(100vh - 64px)' }}>
+  <Sidebar labels={LABELS} />
+  <div style={{ flex: 1, padding: '16px', overflowY: 'auto' }}>
+    <div data-testid='annotate-container'>
+      Annotation Container will go here
+    </div>
+  </div>
+</div>
+    </div>
+  );
 };
 
 export default AnnotateTemplate;
+
+
