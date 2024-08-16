2d06999d105a48f0f06cf02eb4006942a7d60002
Arindam Kulshi <akulshi04@gmail.com>
2024-08-16 09:22:38 -0700

    used accordian

--- OCR/frontend/src/pages/AnnotateTemplate.tsx
+++ OCR/frontend/src/pages/AnnotateTemplate.tsx
@@ -5,35 +5,37 @@ import { Stepper } from "../componets/Stepper.tsx";
 import { AnnotateStep } from "../utils/constants.ts";
 import { useFiles } from "../contexts/FilesContext.tsx";
 import * as pdfjsLib from "pdfjs-dist";
-import Sidebar from '../componets/Sidebar';
+import { Accordion, AccordionItemProps } from '@trussworks/react-uswds';
 import { LABELS } from '../constants/labels';
 
+interface LabelItem {
+  name: string;
+  required: boolean;
+  color: string;
+  subItems?: LabelItem[];
+}
+
+interface LabelCategory {
+  title: string;
+  items: LabelItem[];
+}
+
 const AnnotateTemplate: React.FC = () => {
-
-
   const [images, setImages] = useState<string[]>([]);
-
   const { files } = useFiles();
   const pdfFile = files[0];
-  if (pdfFile === undefined) {
-    // navigate back to upload
-  }
-
 
   useEffect(() => {
-
     if (!(pdfFile instanceof File)) {
       console.error("pdfFile is not a valid File object");
       return;
     }
 
-
-    //CDN works, todo investigate a way to circumvent this issue
     pdfjsLib.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.min.mjs`;
 
     const convertPdfToImages = async (file: File) => {
       const images: Array<string> = [];
-      const data = URL.createObjectURL(file)
+      const data = URL.createObjectURL(file);
       const pdf = await pdfjsLib.getDocument(data).promise;
       const canvas = document.createElement("canvas");
       for (let i = 0; i < pdf.numPages; i++) {
@@ -48,16 +50,41 @@ const AnnotateTemplate: React.FC = () => {
       canvas.remove();
       URL.revokeObjectURL(data);
       return images;
-    }
+    };
 
     convertPdfToImages(pdfFile).then((imgs) => {
       setImages(imgs);
-      console.log(imgs)
     });
+  }, [files]);
 
-  }, [files])
+  const renderLabelContent = (category: LabelCategory): JSX.Element => (
+    <ul className="usa-list usa-list--unstyled">
+      {category.items.map((item) => (
+        <li key={item.name}>
+            {item.name} {!item.required && <span>Not found</span>}
+          {item.subItems && (
+            <ul className="usa-list usa-list--unstyled margin-left-4">
+              {item.subItems.map((subItem) => (
+                <li>
+                  <span>{subItem.name}</span>
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
 
-  console.log(images)
+  const accordionItems: AccordionItemProps[] = Object.entries(LABELS).map(([key, category]) => ({
+    title: category.title,
+    content: renderLabelContent(category),
+    expanded: false,
+    id: key,
+    headingLevel: 'h3'
+  }));
+
+  
 
   return (
     <div className="display-flex flex-column flex-justify-start width-full height-full padding-1 padding-top-2">
@@ -67,14 +94,14 @@ const AnnotateTemplate: React.FC = () => {
         <Stepper currentStep={AnnotateStep.Annotate} />
       </div>
       <Divider margin="0px" />
-      <div className="display-flex flex-row flex-1 overflow-hidden">
-        <Sidebar labels={LABELS} />
-        <div className="flex-1 padding-2">
-          <div className="text-left">
-            <h2>Segment and label</h2>
-            <p className="text-base">Annotate by segmenting and labeling your new template.</p>
-          </div>
+      <div className="grid-row height-full">
+        <div className="grid-col flex-3">
+          <h2>Segment and label</h2>
+          <p className="text-base">Annotate by segmenting and labeling your new template.</p>
           <Divider margin="0px" />
+          <Accordion items={accordionItems} />
+        </div>
+        <div className="grid-col flex-7">
           <div className="height-full">
             {pdfFile instanceof File ? (
               <iframe className="width-full height-full" src={URL.createObjectURL(pdfFile)}></iframe>
@@ -89,5 +116,3 @@ const AnnotateTemplate: React.FC = () => {
 };
 
 export default AnnotateTemplate;
-
-
