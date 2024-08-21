61aef3fe0228be190d25f04d168053c993b0b905
Zedd Shmais <zedd@skylight.digital>
2024-08-21 10:38:59 -0500

    fix issues with main

--- OCR/frontend/vite.config.ts
+++ OCR/frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import {configDefaults} from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/setup.js',
+    exclude: [
+      ...configDefaults.exclude,
+      '**/e2e/**',
+    ],
   },
   css: {
     preprocessorOptions: {
