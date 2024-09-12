af429b9c806d1f639406a49c66596fc82174ecb5
Andrew Schreiber <schreiaj@users.noreply.github.com>
2024-09-12 14:46:25 -0500

    Schreiaj/blank main page (#221)
    * Add root page with structure in place.
    
    Added playwright tests to ensure behavior
    Updated dev docker image to support playwright which required moving to
    a Debian based base image (Alpine linux base image
    is smaller but doesn't work well with Chrome runner because Chrome
    doesn't publish a precompiled package for it in Alpine)
    
    * Remove inline styles
    
    Remove commented out code
    
    * Update Playwright tests to work in CI
    
    Parameterize baseURL so we can just work with internal routes

--- OCR/frontend/src/App.tsx
+++ OCR/frontend/src/App.tsx
@@ -1,37 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.scss'
-import { Button } from '@trussworks/react-uswds'
+import {Button, Link, SideNav} from "@trussworks/react-uswds";
+import {useLocation, useNavigate, useNavigation} from "react-router-dom";
 
+import {AppHeader} from "./components/AppHeader/AppHeader.tsx";
+import dataLinkLogo from "./assets/datalink_placeholder_logo.svg";
+import extractImage from "./assets/extract_image.svg"
 function App() {
-  const [count, setCount] = useState(0)
+    const {pathname} = useLocation()
+    const navigate = useNavigate()
+    const navLinks = [
+        {text: "Annotate and Extract", url: "/annotate"},
+        {text: "Label Management", url: "/labels"},
+        {text: "Dashboard", url: "/"}
+    ]
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Button type="button">Click Me</Button>
-    </>
-  )
+
+    return (
+        <>
+            <div className='display-flex flex-column width-full height-full'>
+                <AppHeader jurisdiction={`Demo STLT`}/>
+                <div className="display-flex flex-row height-full">
+                    <div
+                        className="flex-3 padding-top-1 padding-left-1 padding-right-4 minw-15 usa-dark-background bg-primary-dark text-base-lightest display-flex flex-column flex-gap-1 maxw-30"
+                        >
+                        <div className='display-flex flex-column flex-align-start padding-top-2'>
+                            {navLinks.map((i, idx) => {
+                                return <Link key={idx} href={i.url}
+                                             className={`border-left-2px padding-left-1 padding-top-2 padding-bottom-2 ${i.url === pathname ? 'text-bold' : 'border-primary-dark'}`}>{i.text}</Link>
+                            })}
+                        </div>
+                    </div>
+                    <div className="flex-10 display-flex flex-column">
+                        <h2 className="padding-left-2">Annotate and Extract</h2>
+                        <div className=" flex-1 padding-left-2  padding-right-2 bg-idwa-light" >
+                            <p><span className="text-bold">Welcome Blake, </span></p>
+                            <p>Extract data from any PDFs, or images to send to your surveillance systems using data from your saved templates or create new segmentations.</p>
+                            <img className="display-block margin-left-auto margin-right-auto padding-top-8"  src={extractImage} alt="Extract From Documents"/>
+                            <p className="text-bold text-center">You have no segmentation templates set up yet. The templates will be used to extract data for your lab forms.</p>
+                            <Button type="button" className="usa-button display-flex flex-align-center margin-left-auto margin-right-auto" onClick={() => navigate("/new-template/upload")}>+ New Template</Button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </>
+    )
 }
 
 export default App
