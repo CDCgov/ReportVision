bdbe7943f299702d3a61ac37132c5fd44bfb6815
Andrew Schreiber <schreiaj@gmail.com>
2024-09-12 13:10:15 -0500

    Update Playwright tests to work in CI
    Parameterize baseURL so we can just work with internal routes

--- OCR/frontend/e2e/App.spec.ts
+++ OCR/frontend/e2e/App.spec.ts
@@ -3,14 +3,15 @@ import {Simulate} from "react-dom/test-utils";
 import click = Simulate.click;
 
 test('has STLT Name', async ({ page }) => {
-    await page.goto('http://localhost:5173/');
+    await page.goto('/');
 
     await expect(page.getByText('Demo STLT')).toBeVisible()
 });
 
-test('has new template button', async ({ page }) => {
-    await page.goto('http://localhost:5173/');
+test('has new template button', async ({ page , baseURL}) => {
+    await page.goto('/');
     await expect(page.getByRole('button', { name: '+ New Template' })).toBeVisible()
     await page.getByRole('button', { name: '+ New Template' }).click()
-    expect(page.url()).toEqual("http://localhost:5173/new-template/upload")
+    expect(page.url()).toEqual(`${baseURL}/new-template/upload`)
+
 });
