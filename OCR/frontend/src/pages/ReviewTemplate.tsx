22f6f48a3a95b394e04539a2a989db8c0d27165e
Arindam Kulshi <akulshi04@gmail.com>
2024-09-16 16:36:10 -0700

    added ReviewTemplate logic

--- OCR/frontend/src/pages/ReviewTemplate.tsx
+++ OCR/frontend/src/pages/ReviewTemplate.tsx
@@ -1,18 +1,59 @@
-import { Divider } from "../components/Divider";
 import { ExtractStep } from "../utils/constants";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ExtractDataHeader from "../components/ExtractDataHeader";
 import React from "react";
 import { ExtractStepper } from "../components/ExtractStepper";
+import { Table } from "@trussworks/react-uswds";
+import { Divider } from "../components/Divider";
+import documentImage from "./SyphForm.png";
+
+interface Result {
+  text: string;
+  confidence: number;
+}
+
+interface SubmissionData {
+  template_name: string;
+  file_image: string;
+  results: {
+    [key: string]: Result;
+  };
+}
 
 const ReviewTemplate: React.FC = () => {
   const navigate = useNavigate();
+  const [submissionData, setSubmissionData] = useState<SubmissionData | null>(
+    null
+  );
+
+  const fakeData = {
+    template_name: "Syph Template",
+    file_image: documentImage,
+    results: {
+      Name: {
+        text: "TESTPATIENT,13",
+        confidence: 98.75,
+      },
+      Patient_ID: {
+        text: "12090546",
+        confidence: 95.42,
+      },
+      Age: {
+        text: "5",
+        confidence: 97.1,
+      },
+    },
+  };
 
   useEffect(() => {
-    fetch("/api/ocr-result")
-      .then((response) => response.json())
-      .then(() => {});
+    const data = localStorage.getItem("submission");
+    if (data) {
+      setSubmissionData(JSON.parse(data));
+    } else {
+      // use fakedata if no data
+      setSubmissionData(fakeData);
+    }
   }, []);
 
   const handleBack = () => {
@@ -23,18 +64,55 @@ const ReviewTemplate: React.FC = () => {
     navigate("/extract/submit");
   };
 
+  const handleClose = () => {
+    navigate("/");
+  };
+
+  //fallback if no valid template is available can edit if needed
+  if (!submissionData) {
+    return <div>Loading...</div>;
+  }
+
+  const { file_image, results } = submissionData;
+
   return (
     <div className="display-flex flex-column flex-justify-start width-full height-full padding-1 padding-top-2">
       <ExtractDataHeader
-        onBack={handleBack}
         onSubmit={handleSubmit}
+        onExit={handleClose}
+        onBack={handleBack}
         isUploadComplete={true}
       />
       <Divider margin="0px" />
       <div className="display-flex flex-justify-center padding-top-4">
         <ExtractStepper currentStep={ExtractStep.Review} />
       </div>
-      <Divider margin="0px" />
+
+      <div className="display-flex flex-justify-between padding-top-4">
+        <div className="width-50">
+          <Table fullWidth striped>
+            <thead>
+              <tr>
+                <th>Label</th>
+                <th>Value</th>
+                <th>Label CS</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(results).map(([key, value]) => (
+                <tr key={key}>
+                  <td>{key}</td>
+                  <td>{value.text}</td>
+                  <td>{value.confidence}%</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+        <div className="width-50">
+          <img src={file_image} alt="Document" style={{ width: "100%" }} />
+        </div>
+      </div>
     </div>
   );
 };
