5c7edb30168a0437f1213e3401d7698eec6163d8
Andrew Schreiber <schreiaj@users.noreply.github.com>
2024-09-17 11:11:23 -0500

    Create Template Index (#230)
    * Create Template Index
    
    Created helper methods in the console to manipulate the localStorage of
    templates
    
    Created a SortableTable component if we need this in other places.
    A nice todo on it would be pass it a data fetch method and use
    `useQuery` to allow it to push the data sorting to server side and allow
    proper pagination
    
    * Correct spacing on sort arrows
    
    * Unregister template console commands on unload
    
    * Add e2e tests
    
    * Remove unused deps

--- OCR/frontend/e2e/App.spec.ts
+++ OCR/frontend/e2e/App.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import {Simulate} from "react-dom/test-utils";
 import click = Simulate.click;
+import {describe} from "vitest";
 
 test('has STLT Name', async ({ page }) => {
     await page.goto('/');
@@ -15,3 +16,67 @@ test('has new template button', async ({ page , baseURL}) => {
     expect(page.url()).toEqual(`${baseURL}/new-template/upload`)
 
 });
+
+test.describe('when templates exist', async () => {
+    test.beforeEach(async ({page}) => {
+        await page.goto('/')
+        await page.evaluate(() => {
+            const templates = [
+
+                {
+                    name: "MumpsQuestV1",
+                    lab: "Quest",
+                    createdBy: "J.Smith",
+                    status: "Completed",
+                    lastUpdated: new Date(Date.parse("2025-03-24"))
+                },
+                {
+                    name: "LBTIRadar",
+                    lab: "Radar",
+                    createdBy: "C.Alex",
+                    status: "Completed",
+                    lastUpdated: new Date(Date.parse("2025-05-30"))
+                },
+                {
+                    name: "COVIDBaylor1",
+                    lab: "Emory",
+                    createdBy: "A.Bryant",
+                    status: "Completed",
+                    lastUpdated: new Date(Date.parse("2025-06-21"))
+                },
+                {
+                    name: "COVIDEMory",
+                    lab: "Baylor",
+                    createdBy: "D.Smith",
+                    status: "Completed",
+                    lastUpdated: new Date(Date.parse("2024-06-21"))
+                },
+            ];
+            localStorage.setItem('templates', JSON.stringify(templates))
+        })
+    })
+    test('displays list of templates if they exist and sorting functions', async ({page, baseURL}) => {
+        await page.goto('/')
+        await expect(page.getByRole('heading', { name: 'Saved Templates' })).toBeVisible()
+        await expect(page.locator('tbody').getByRole('row')).toHaveCount(4)
+        await page.getByText('Updated On').click()
+        await expect(page.locator('tbody').locator('tr').nth(0).getByRole('cell').nth(1)).toHaveText('6/21/2024')
+        await page.getByText('Updated On').click()
+        await expect(page.locator('tbody').locator('tr').nth(0).getByRole('cell').nth(1)).toHaveText('6/21/2025')
+        await page.close()
+
+    })
+    test('has links to extraction', async ({page, baseURL}) => {
+        await page.goto('/')
+        await page.getByRole('button', { name: 'Run Data Extraction' }).click()
+        expect(page.url()).toEqual(`${baseURL}/extract/upload`)
+
+    })
+    test('has links to create new template', async ({page, baseURL}) => {
+        await page.goto('/')
+        await page.getByRole('button', { name: '+ New Template' }).click()
+        expect(page.url()).toEqual(`${baseURL}/new-template/upload`)
+
+    })
+})
+
