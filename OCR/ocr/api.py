7528d2709e7f381353620fe066bdf4b0475b25fb
Kevin Nguyen <knguyen@rise8.us>
2024-09-19 14:53:38 -0500

    feat(185): Add Cors, update shape type
    feat(185): Add Cors, update shape type

--- OCR/ocr/api.py
+++ OCR/ocr/api.py
@@ -34,6 +34,10 @@ ocr = ImageOCR()
 async def health_check():
     return {"status": "UP"}
 
+@app.get("/test")
+async def read_root():
+    return {"message": "Hello CORS enabled API!"}
+
 
 @app.post("/image_file_to_text/")
 async def image_file_to_text(source_image: UploadFile, segmentation_template: UploadFile, labels: str = Form()):
