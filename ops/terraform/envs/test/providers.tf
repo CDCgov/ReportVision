43ffcf8ea706e91f47f18e8e2641f29b69a62255
Administrator <gitlab_admin_731e3f@example.com>
2024-09-20 14:25:28 -0600

    tfstate stuff

--- ops/terraform/envs/test/providers.tf
+++ ops/terraform/envs/test/providers.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "idwa-rv-dev-rg"
+    storage_account_name = "tfstatervdev1540"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
