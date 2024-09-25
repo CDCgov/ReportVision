13f338f2c073ac04761d1f08713b4a48e23a8374
Kevin Nguyen <knguyen@rise8.us>
2024-09-25 16:05:48 -0500

    feat(185): Add pdf image, api implementation

--- OCR/frontend/api/api.ts
+++ OCR/frontend/api/api.ts
@@ -3,16 +3,17 @@ import { ImageToTextArgs, ImageToTextResponse } from "./types/types";
 export const AddFormData = async (args: ImageToTextArgs): Promise<ImageToTextResponse | null> => {
 
     const { sourceImage, templateImage, fieldNames } = args;
-    const form = new FormData();
-    form.append("source_image", sourceImage);
-    form.append("segmentation_template", templateImage);
-    form.append("labels", JSON.stringify(fieldNames));
-
+    const form = new URLSearchParams({
+        source_image: sourceImage,
+        segmentation_template: templateImage,
+        labels: JSON.stringify(fieldNames),
+      });
+      
     try {
-        const response = await fetch("http://localhost:8000/image_file_to_text/", {
+        const response = await fetch("http://localhost:8000/image_to_text/", {
             "method": "POST",
             "headers": {
-              "Content-Type": "multipart/form-data",
+              "Content-Type": "application/x-www-form-urlencoded"
             },
             body: form
           })
