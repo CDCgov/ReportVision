f4e39be8281c787d5f4f87bc3727e1901bdcf8c1
Administrator <gitlab_admin_731e3f@example.com>
2024-10-15 14:12:40 -0600

    moved everything to base

--- ops/terraform/variables.tf
+++ ops/terraform/variables.tf
@@ -2,22 +2,5 @@ variable "resource_group_name" {
   description = "value of the Azure resource group to deploy to"
 }
 
-variable "subscription_id" {
-  description = "value of the Azure Subscription ID to use"
-}
-
-variable "client_id" {
-  description = "Client ID"
-}
-
-variable "docker_tag" {
-}
-
-variable "docker_registry_path" {
-}
-
-variable "docker_registry_url" {
-}
-
 variable "name" {
 }
