cba27f135c2113a454ee0ad4f90d971d1a45389f
Arindam Kulshi <arindam@skylight.digital>
2024-10-20 10:40:33 -0700

    linting

--- OCR/ocr/metrics_main.py
+++ OCR/ocr/metrics_main.py
@@ -1,27 +1,25 @@
 from services.metrics_analysis import OCRMetrics
 import os
 
+
 def main():
     current_script_dir = os.path.dirname(os.path.abspath(__file__))
 
     file_relative_path_ground_truth = ".."
     file_relative_path_ocr = ".."
 
-
     ground_truth_json_path = os.path.join(current_script_dir, file_relative_path_ground_truth)
     ocr_json_path = os.path.join(current_script_dir, file_relative_path_ocr)
 
-
     ocr_metrics = OCRMetrics(ocr_json_path, ground_truth_json_path)
 
-
     metrics = ocr_metrics.calculate_metrics()
-
 
     total_metrics = ocr_metrics.total_metrics(metrics)
 
     output_csv_path = os.path.join(current_script_dir, "metrics_file.csv")
     OCRMetrics.save_metrics_to_csv(metrics, total_metrics, output_csv_path)
+
 
 if __name__ == "__main__":
     main()
