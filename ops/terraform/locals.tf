1574a1b33e179371ae81f18632c9a5e1596cee6f
Administrator <gitlab_admin_731e3f@example.com>
2024-10-24 17:06:41 -0600

    Create unique URL for demo environment

--- ops/terraform/locals.tf
+++ ops/terraform/locals.tf
@@ -52,4 +52,12 @@ locals {
       lbsubnetcidr  = "10.6.3.0/24"
     }
   }
+  demo = {
+    demo = {
+      vnetcidr      = "10.7.0.0/16"
+      appsubnetcidr = "10.7.1.0/24"
+      websubnetcidr = "10.7.2.0/24"
+      lbsubnetcidr  = "10.7.3.0/24"
+    }
+  }
 }
