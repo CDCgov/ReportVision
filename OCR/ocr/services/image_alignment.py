d4efa3d54b59c65aae7b445bad1899e5e66c8995
Jonathan Chang <jonathan@skylight.digital>
2024-10-25 15:53:13 -0700

    Document new alignment function

--- OCR/ocr/services/image_alignment.py
+++ OCR/ocr/services/image_alignment.py
@@ -1,4 +1,3 @@
-import cv2 as cv
 import numpy as np
 
 from ocr.services.alignment import ImageHomography
@@ -9,8 +8,12 @@ class ImageAligner:
         self.aligner = aligner
 
     def align(self, source_image: np.ndarray, template_image: np.ndarray) -> np.ndarray:
+        """
+        Aligns an image using the specified image alignment backend.
+
+        source_image: the image to be aligned, as a numpy ndarray.
+        template_image: the image that `source_image` will be aligned against, as a numpy ndarray.
+            May not be used for all image alignment backends.
+        """
         aligned_image = self.aligner.align(source_image, template_image)
-        cv.imwrite("aligned.png", aligned_image)
-        cv.imwrite("template.png", template_image)
-        cv.imwrite("raw.png", source_image)
         return aligned_image
