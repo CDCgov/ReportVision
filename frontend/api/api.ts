fe808d7ba5432077cd09fe4aff479351334af307
Jonathan Chang <jonathan@skylight.digital>
2024-11-04 14:46:37 -0800

    Add frontend API function and interface types

--- frontend/api/api.ts
+++ frontend/api/api.ts
@@ -1,4 +1,28 @@
-import { ImageToTextArgs, ImageToTextResponse } from "./types/types";
+import { ImageToTextArgs, ImageToTextResponse, AlignImageArgs, AlignImageResponse } from "./types/types";
+
+export const AlignImage = async (args: AlignImageArgs): Promise<AlignImageResponse | null> => {
+    const { sourceImage, templateImage } = args;
+    const form = new URLSearchParams({
+        source_image: sourceImage,
+        segmentation_template: templateImage,
+      });
+
+    const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8000/"
+    const alignImageURL = `${apiUrl}image_alignment/`
+    try {
+        const response = await fetch(alignImageURL, {
+            "method": "POST",
+            "headers": {
+              "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: form
+          })
+          return await response.json() as AlignImageResponse;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
 
 export const ImageToText = async (args: ImageToTextArgs): Promise<ImageToTextResponse | null> => {
 
