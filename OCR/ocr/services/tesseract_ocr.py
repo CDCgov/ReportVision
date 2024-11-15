93ce87c93a47bf79bb4dfa64d5bca7e98d420bb6
Jonathan Chang <jonathan@skylight.digital>
2024-11-15 15:22:53 -0800

    Guess at tessdata path

--- OCR/ocr/services/tesseract_ocr.py
+++ OCR/ocr/services/tesseract_ocr.py
@@ -1,12 +1,35 @@
+import pathlib
+import os
+
 import tesserocr
 import numpy as np
 from PIL import Image
 
 
 class TesseractOCR:
+    @staticmethod
+    def _guess_tessdata_path():
+        candidate_paths = [
+                "/usr/local/share/tesseract/",      # Default local install
+                "/usr/share/tesseract/",            # Red Hat
+                "/usr/share/tesseract-ocr/4.00",    # Ubuntu
+                "/opt/homebrew/share",              # macOS (Homebrew)
+                "/opt/local/share"                  # macOS (MacPorts)
+                ]
+
+        if "TESSDATA_PREFIX" in os.environ:
+            return os.environ["TESSDATA_PREFIX"]
+
+        for path in candidate_paths:
+            tessdata = pathlib.Path(path) / "tessdata"
+            if tessdata.exists():
+                return bytes(tessdata)
+
+        return None
+
     def image_to_text(self, segments: dict[str, np.ndarray]) -> dict[str, tuple[str, float]]:
         digitized: dict[str, tuple[str, float]] = {}
-        with tesserocr.PyTessBaseAPI() as api:
+        with tesserocr.PyTessBaseAPI(path=self._guess_tessdata_path()) as api:
             for label, image in segments.items():
                 if image is None:
                     continue
