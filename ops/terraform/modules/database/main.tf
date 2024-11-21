78e199c52801d819711d1b07e53d661011950c87
marycrawford <mary.crawford@skylight.digital>
2024-11-20 22:15:51 -0800

    remove azurerm_postgresql_server code

--- ops/terraform/modules/database/main.tf
+++ ops/terraform/modules/database/main.tf
@@ -1,26 +1,3 @@
-# # PostgreSQL Server
-# resource "azurerm_postgresql_server" "postgres_server" {
-#   name                         = "reportvisionpgserver"
-#   location                     = var.location
-#   resource_group_name          = var.resource_group_name
-#   sku_name                     = var.sku_name
-#   version                      = var.engine_version
-#   administrator_login          = var.db_username
-#   administrator_login_password = random_string.setup_rds_password.result
-#   storage_mb                   = 5120 # 5 GB storage
-#   backup_retention_days        = 7
-#   ssl_enforcement_enabled      = true
-# }
-
-# # PostgreSQL Database
-# resource "azurerm_postgresql_database" "postgres_db" {
-#   name                = "postgresdb"
-#   resource_group_name = var.resource_group_name
-#   server_name         = azurerm_postgresql_server.postgres_server.name
-#   charset             = "UTF8"
-#   collation           = "English_United Kingdom.1252"
-# }
-
 # Azure Postgres Single Service (azurerm_postgresql_server) retires in March 2025.  
 # As a result we are using Azure Database for PostgreSQL Flexible Server
 # with granular control, flexibility and better cost optimization. 
