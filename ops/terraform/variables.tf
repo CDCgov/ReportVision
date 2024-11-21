6b81cc22929aaae61e3292c0ece605d451ce4079
marycrawford <mary.crawford@skylight.digital>
2024-11-20 22:52:00 -0800

    remove variables used when working on vault module

--- ops/terraform/variables.tf
+++ ops/terraform/variables.tf
@@ -1,29 +1,8 @@
-variable "azure_tenant_id" {
-  description = "Unique Identifier for the Azure AD tenant for the app"
-}
-
-variable "client_id" {}
-variable "client_secret" {
-
-}
-variable "tenant_id" {
-
-}
 variable "name" {}
-
-variable "object_id" {
-  type        = string
-  description = "The Azure Object ID"
-}
 
 variable "sku_name" {
   type        = string
   description = "The Azure Stock Keep Unit (SKU) version"
-}
-
-variable "vite_api_url" {
-  type        = string
-  description = "The application API Url"
 }
 
 variable "resource_group_name" {
