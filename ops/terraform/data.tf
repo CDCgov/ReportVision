4f33026c9abea8794bf12975b2782ea33fabd68e
Administrator <gitlab_admin_731e3f@example.com>
2024-11-26 11:43:09 -0700

    rm vault data source secrets

--- ops/terraform/data.tf
+++ ops/terraform/data.tf
@@ -1,22 +1,3 @@
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
-
-data "azurerm_resource_group" "rg_global" {
-  name = "${var.name}-rg-management"
-}
-
-data "azurerm_key_vault" "global" {
-  name                = "simple-report-global"
-  resource_group_name = data.azurerm_resource_group.rg_global.name
-}
-
-data "azurerm_key_vault_secret" "rv_db_jdbc" {
-  name         = "reportvision-${terraform.workspace}-db-jdbc"
-  key_vault_id = data.azurerm_key_vault.global.id
-}
-
-data "azurerm_key_vault_secret" "postgres_password" {
-  name         = "reportvision-${terraform.workspace}-db-password"
-  key_vault_id = data.azurerm_key_vault.global.id
-}
