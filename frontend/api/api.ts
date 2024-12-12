f198c5c4863751d283660ca1833b24129895e19d
Arindam Kulshi <arindam@skylight.digital>
2024-12-12 13:36:58 -0800

    updated middleware URL

--- frontend/api/api.ts
+++ frontend/api/api.ts
@@ -6,8 +6,7 @@ import {
 } from "./types/types";
 
 const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8000/";
-const middlewareURL =
-  import.meta.env.VITE_MIDDLEWARE_URL || "http://localhost:8000/";
+const middlewareURL =  import.meta.env.MIDDLEWARE_API_URL || "http://localhost:8000/";
 
 export const AlignImage = async (
   args: AlignImageArgs,
@@ -45,7 +44,7 @@ export const ImageToText = async (
     labels: JSON.stringify(fieldNames),
   });
 
-  const imageToTextURL = `${apiUrl}image_to_text/`;
+  const imageToTextURL = `${middlewareURL}/api/image_file_to_text/`;
   try {
     const response = await fetch(imageToTextURL, {
       method: "POST",
