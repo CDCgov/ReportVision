d6ebc0416ee446bf4c0ceeeaece5187331fa376f
Administrator <gitlab_admin_731e3f@example.com>
2024-12-12 12:27:40 -0700

    deploy middleware to dev

--- ops/terraform/main.tf
+++ ops/terraform/main.tf
@@ -81,7 +81,7 @@ module "middleware_api" {
     POSTGRES_DB       = module.database.postgres_db_name
     POSTGRES_USER     = module.database.postgres_user
     POSTGRES_PASSWORD = module.vault.postgres_password
-    WEBSITES_CONTAINER_START_TIME_LIMIT = 500
+    WEBSITES_CONTAINER_START_TIME_LIMIT = 400
     DEBUG = true
   }
 
