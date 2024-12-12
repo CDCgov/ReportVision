c9b568ff944258c577d0a2fbd584695806d01ccc
marycrawford <mary.crawford@skylight.digital>
2024-12-12 03:39:34 -0800

    update vault resource names and attributes

--- ops/terraform/modules/vault/main.tf
+++ ops/terraform/modules/vault/main.tf
@@ -1,10 +1,11 @@
 resource "azurerm_key_vault" "this" {
-  name                     = "${var.name}vault${var.env}"
-  location                 = var.location
-  resource_group_name      = var.resource_group_name
-  sku_name                 = "standard"
-  tenant_id                = data.azurerm_client_config.current.tenant_id
-  purge_protection_enabled = false
+  name                       = "${var.name}vault${var.env}"
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+  sku_name                   = "standard"
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  purge_protection_enabled   = false
+  soft_delete_retention_days = 7
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
--- ops/terraform/providers.tf
+++ ops/terraform/providers.tf
@@ -19,11 +19,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
+  features {}
+  # features {
+  #   key_vault {
+  #     recover_soft_deleted_key_vaults = true
+  #   }
+  # }
 }
 
